3961344ec7407bfe887b6e60e67e348f9971dc43
Moses Palmér <mosespalmer@gmail.com>
2018-02-01 10:48:20 +0100

    Implemented getattr

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -3,6 +3,7 @@ use std::path;
 use std::sync;
 
 use fuse_mt;
+use libc;
 
 use data;
 
@@ -85,5 +86,133 @@ impl MediaFS {
 impl fuse_mt::FilesystemMT for MediaFS {
     fn init(&self, _req: fuse_mt::RequestInfo) -> fuse_mt::ResultEmpty {
         Ok(())
+    }
+
+    fn getattr(
+        &self,
+        req: fuse_mt::RequestInfo,
+        path: &path::Path,
+        _fh: Option<u64>,
+    ) -> fuse_mt::ResultEntry {
+        let result: fuse_mt::ResultEntry = lookup!(cache!(self.cache), &path)
+            .into();
+        result.map(|(ttl, fa)| (ttl, fa.for_user(req.uid, req.gid)))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::os::unix::fs::PermissionsExt;
+
+    use fuse;
+    use tempdir;
+
+    use super::*;
+    use data::tests::*;
+
+
+    /// Tests that getattr returns the expected data.
+    #[test]
+    fn test_getattr() {
+        let data = "hello world";
+        let (mount_point, _source_dir, _session, paths) =
+            mount(vec![("test.jpg", data, 2000, 1, 1)].into_iter());
+
+        let (ref source, ref target) = paths[0];
+        let source_meta = source.metadata().unwrap();
+        let target_meta = target.metadata().unwrap();
+        assert_eq!(
+            io::Error::from_raw_os_error(libc::ENOENT).kind(),
+            mount_point.path().join(
+                "invalid/path"
+            ).metadata().unwrap_err().kind(),
+        );
+        assert_eq!(
+            0o444,
+            target_meta.permissions().mode() & 0o777,
+        );
+        assert_eq!(
+            source_meta.len(),
+            target_meta.len(),
+        );
+    }
+
+    /// An item to populate a file system.
+    ///
+    /// This is the tuple `(name, data, year, moth, day)`.
+    type MountItem = (&'static str, &'static str, i32, i32, i32);
+
+    /// The result of a mount operation.
+    ///
+    /// This is the tuple `(mount_point, source_dir, background_session,
+    /// source_and_target_paths)`
+    type MountResult<'a> = (tempdir::TempDir,
+                            tempdir::TempDir,
+                            fuse::BackgroundSession<'a>,
+                            Vec<(path::PathBuf, path::PathBuf)>);
+
+    /// Mounts a file system on a temporary mount point.
+    ///
+    /// The mount point and a temporary directory is returned along with a
+    /// background session.
+    ///
+    /// # Arguments
+    /// *  `items` - A sequence of
+    ///    used to populate the file system.
+    fn mount<'a, T: Iterator<Item = MountItem>>(items: T) -> MountResult<'a> {
+        // Create temporary directories and the file system handler
+        let mount_point = tempdir::TempDir::new(&"medifs-mount").unwrap();
+        let source_dir = tempdir::TempDir::new(&"medifs-source").unwrap();
+        let mediafs = MediaFS::new("timestamped".into());
+
+        // Add all items
+        let source_and_target_paths = items
+            .map(|(name, data, year, month, day)| {
+                let path = source_dir.path().join(name);
+                (
+                    path.clone(),
+                    mount_point.path().join(
+                        mediafs
+                            .add(item_with_data(
+                                path,
+                                data.as_bytes(),
+                                year,
+                                month,
+                                day,
+                            ))
+                            .unwrap(),
+                    ),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        // Actually mount the file system
+        let background_session = unsafe {
+            fuse_mt::spawn_mount(
+                fuse_mt::FuseMT::new(mediafs, 1),
+                &mount_point,
+                &[],
+            ).unwrap()
+        };
+
+        (
+            mount_point,
+            source_dir,
+            background_session,
+            source_and_target_paths,
+        )
+    }
+
+    /// Reads the full content of a file into a buffer.
+    ///
+    /// # Arguments
+    /// *  `path` - The file path.
+    fn read_file<P: AsRef<path::Path>>(path: P) -> Vec<u8> {
+        let mut file = fs::File::open(&path).unwrap();
+        let mut result = vec![0u8; file.metadata().unwrap().len() as usize];
+        file.read_to_end(&mut result).unwrap();
+
+        result
     }
 }
--- src/files/traits.rs
+++ src/files/traits.rs
@@ -1,1 +1,79 @@
+use fuse_mt;
 
+use time;
+
+use data;
+use super::util;
+
+
+/// Allows changing the owner of a resource.
+pub trait ForUser {
+    /// Changes the owner and group of this item.
+    ///
+    /// # Arguments
+    /// *  `uid` - The user ID.
+    /// *  `gid` - The group ID.
+    fn for_user(self, uid: u32, gid: u32) -> Self;
+}
+
+
+impl<'a> From<&'a data::Entry> for fuse_mt::ResultEntry {
+    fn from(source: &'a data::Entry) -> fuse_mt::ResultEntry {
+        let ttl = time::Timespec::new(0x7FFFFFFF, 0);
+        let timestamp = source.timestamp();
+        match source {
+            &data::Entry::Directory(_) => {
+                Ok((
+                    ttl,
+                    fuse_mt::FileAttr {
+                        size: 0,
+                        blocks: 0,
+                        atime: timestamp,
+                        mtime: timestamp,
+                        ctime: timestamp,
+                        crtime: timestamp,
+                        kind: fuse_mt::FileType::Directory,
+                        perm: 0o555,
+                        nlink: 1,
+                        uid: 0,
+                        gid: 0,
+                        rdev: 0,
+                        flags: 0,
+                    },
+                ))
+            }
+            &data::Entry::Item(ref item) => {
+                item.path
+                    .metadata()
+                    .map(|meta| {
+                        (
+                            ttl,
+                            fuse_mt::FileAttr {
+                                size: meta.len(),
+                                blocks: 0,
+                                atime: timestamp,
+                                mtime: timestamp,
+                                ctime: timestamp,
+                                crtime: timestamp,
+                                kind: fuse_mt::FileType::RegularFile,
+                                perm: 0o444,
+                                nlink: 1,
+                                uid: 0,
+                                gid: 0,
+                                rdev: 0,
+                                flags: 0,
+                            },
+                        )
+                    })
+                    .map_err(util::map_error)
+            }
+        }
+
+    }
+}
+
+impl ForUser for fuse_mt::FileAttr {
+    fn for_user(self, uid: u32, gid: u32) -> Self {
+        Self { uid, gid, ..self }
+    }
+}
