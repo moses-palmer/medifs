152c8471d69d36916a810d1cb2c2301a2d569856
Moses Palmér <mosespalmer@gmail.com>
2018-02-04 12:04:03 +0100

    Pass mount point on command line

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,35 @@ extern crate time;
 #[cfg(test)]
 extern crate tempdir;
 
-use std::path::Path;
 use std::process;
 
 mod data;
 mod files;
+mod types;
 
 
 fn main() {
-    let path = Path::new(".");
+    let matches = clap::App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            clap::Arg::with_name("MOUNT_POINT")
+                .help("The target mount point.")
+                .required(true),
+        )
+        .get_matches();
+
+    let mount_point = matches
+        .value_of("MOUNT_POINT")
+        .unwrap()
+        .parse::<types::MountPoint>()
+        .unwrap_or_else(|e| { e.exit(); });
     let mediafs = files::MediaFS::new("All".into());
 
     if let Err(e) = fuse_mt::mount(
         fuse_mt::FuseMT::new(mediafs, 1),
-        &path,
+        &mount_point,
         &[],
     )
     {
---
+++ src/types/mount_point.rs
@@ -0,0 +1,41 @@
+use std::path;
+use std::str;
+
+use clap;
+
+
+pub struct MountPoint(path::PathBuf);
+
+impl str::FromStr for MountPoint {
+    type Err = clap::Error;
+
+    fn from_str(source: &str) -> Result<Self, Self::Err> {
+        let path = path::PathBuf::from(source);
+        if !path.is_dir() {
+            Err(clap::Error::with_description(
+                format!("{} is not a directory", source).as_str(),
+                clap::ErrorKind::InvalidValue,
+            ))
+        } else if let Ok(dir) = path.read_dir() {
+            if dir.count() == 0 {
+                Ok(MountPoint(path))
+            } else {
+                Err(clap::Error::with_description(
+                    format!("{} is not an empty directory", source).as_str(),
+                    clap::ErrorKind::InvalidValue,
+                ))
+            }
+        } else {
+            Err(clap::Error::with_description(
+                format!("failed to read {}", source).as_str(),
+                clap::ErrorKind::InvalidValue,
+            ))
+        }
+    }
+}
+
+impl AsRef<path::Path> for MountPoint {
+    fn as_ref(&self) -> &path::Path {
+        &self.0
+    }
+}
