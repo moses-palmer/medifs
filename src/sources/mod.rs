1a7e0db91ffd829fdfe18a78a4bac49e31f9e023
Moses Palmér <mosespalmer@gmail.com>
2018-02-05 09:09:40 +0100

    Added source traits

--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -1,1 +1,12 @@
+use clap;
 
+use files;
+
+
+/// A source of media files.
+pub trait Source {
+    /// Starts this source.
+    ///
+    /// This method should load all items from this source into the cache.
+    fn start(&mut self);
+}
