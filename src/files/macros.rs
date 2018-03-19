022f1b993a9f1c994efccd62201e5f0a0d48fdd5
Moses Palmér <mosespalmer@gmail.com>
2018-03-19 22:53:41 +0100

    Extended documentation for macros

--- src/files/macros.rs
+++ src/files/macros.rs
@@ -1,7 +1,9 @@
 /// Acquires a read lock on a cache.
 ///
-/// If locking fails, this macro will cause the current method to return an
-/// error.
+/// If locking fails, this macro will cause the current method to return
+/// [`libc::EDEADLK`].
+///
+/// [`libc::EDEADLK`]: https://doc.rust-lang.org/libc/x86_64-unknown-linux-gnu/libc/constant.EDEADLK.html
 macro_rules! cache {
     ($cache:expr) => {
         if let Ok(cache) = $cache.read() {
@@ -16,7 +18,9 @@ macro_rules! cache {
 /// Performs a lookup in a cache.
 ///
 /// If no corresponding entry exists, this macro will cause the current method
-/// to return an error.
+/// to return [`libc::ENOENT`].
+///
+/// [`libc::ENOENT`]: https://doc.rust-lang.org/libc/x86_64-unknown-linux-gnu/libc/constant.ENOENT.html
 macro_rules! lookup {
     ($cache:expr, $path:expr) => {
         if let Some(entry) = $cache.lookup($path) {
@@ -30,8 +34,10 @@ macro_rules! lookup {
 
 /// Acquires a write lock on a source and sends a notification.
 ///
-/// If locking fails, this macro will cause the current method to return an
-/// error.
+/// If locking fails, this macro will cause the current method to return
+/// [`libc::EDEADLK`].
+///
+/// [`libc::EDEADLK`]: https://doc.rust-lang.org/libc/x86_64-unknown-linux-gnu/libc/constant.EDEADLK.html
 macro_rules! notify {
     ($source:expr) => {
         if let Ok(ref mut source) = $source.write() {
